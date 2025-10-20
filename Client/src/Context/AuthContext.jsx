b7ac8cd316667cfd6129b7af46676cec17b15b73
PranavThorat1432 <pranavthorat95@gmail.com>
2025-10-20 10:46:30 +0530

    Auth Context

---
+++ Client/src/Context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react'
+export const authContextData = createContext();
+
+const AuthContext = ({children}) => {
+
+    const serverUrl = 'http://localhost:5000'
+    let value = {
+        serverUrl
+    }
+    
+  return (
+    <div>
+        <authContextData.Provider value={value}>
+            {children}
+        </authContextData.Provider>
+    </div>
+  )
+}
+
+export default AuthContext
