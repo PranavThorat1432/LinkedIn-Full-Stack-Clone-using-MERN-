0b55a8e22bebf6271e21e8eee30df2122bdccb48
PranavThorat1432 <pranavthorat95@gmail.com>
2025-10-31 18:00:11 +0530

    Fix bug

--- Server/controllers/authControllers.js
+++ Server/controllers/authControllers.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             secure: process.env.NODE_ENV === 'production'
         });
 
